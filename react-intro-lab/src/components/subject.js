e610b0e3a154490077cc90c2206ea78bed3a8fe9
Shane O' Brien <brienoshane93@gmail.com>
2021-02-17 20:28:39 +0000

    Week 4 Lab Work

---
+++ react-intro-lab/src/components/subject.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Subject(props){
+
+    return (
+        <div className="card">
+            <p>This is a subject! It is called {props.title} and it is in {props.year} of the course. The lecturer is <span className="lecturer">{props.lecturer}</span></p>
+            <p><em>{props.children}</em></p>
+        </div>
+
+    )
+}
+
+export default Subject;
