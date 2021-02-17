c26a6c5c2465fe76c3ef8c019f7facd4a31d1ed3
Shane O' Brien <brienoshane93@gmail.com>
2021-02-17 20:56:29 +0000

    Week 3 Lab Work

--- react-intro-lab/src/components/subject.js
+++ react-intro-lab/src/components/subject.js
@@ -7,7 +7,7 @@ function Subject(props){
             <p>This is a subject! It is called {props.title} and it is in {props.year} of the course. The lecturer is <span className="lecturer">{props.lecturer}</span></p>
             <p><em>{props.children}</em></p>
         </div>
-
+ 
     )
 }
 
