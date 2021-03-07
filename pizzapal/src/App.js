7ac0ba6c5212e1e28e85019a47125e7a2c4d1e6e
Shane O' Brien <brienoshane93@gmail.com>
2021-03-07 20:32:56 +0000

    Pizza Pal Complete

---
+++ pizzapal/src/App.js
@@ -0,0 +1,13 @@
+import 'semantic-ui-css/semantic.min.css';
+import Layout from './components/Layout/Layout';
+import PizzaPal from './containers/PizzaPal/PizzaPal';
+
+function App() {
+  return (
+    <Layout>
+      <PizzaPal />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ pizzapal/src/components/Order/Order.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Grid } from 'semantic-ui-react';
+import Controls from './Controls/Controls';
+import Checkout from './Checkout/Checkout';
+
+const Order = (props) => {
+  return (
+    <Grid.Row columns={2} centered>
+        <Controls menu={props.menu}/>
+        <Checkout />
+    </Grid.Row>
+  )
+};
+
+export default Order;
